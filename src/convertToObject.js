6c0961d54a6e5fc41c31d7eea9d57ad4293e9ebf
Liudmyla Trambai <trambay1301@gmail.com>
2025-03-26 13:20:23 +0100

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,21 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  return sourceString
+    .split(';')
+    .filter((line) => line.includes(':'))
+    .map((line) => {
+      const [key, value] = line.split(':');
+
+      return { [key.trim()]: value.trim() };
+    })
+    .reduce(
+      (accumulatedObj, currentObj) => ({
+        ...accumulatedObj,
+        ...currentObj,
+      }),
+      {},
+    );
 }
 
 module.exports = convertToObject;
